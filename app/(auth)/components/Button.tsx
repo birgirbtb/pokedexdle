5831fe02d6b99e614c19d6faa6f9461ac172988d
marinofranz <me@marino.codes>
2026-01-27 15:41:42 +0000

    add pointer to html button

--- app/(auth)/components/Button.tsx
+++ app/(auth)/components/Button.tsx
@@ -7,7 +7,7 @@ export default function Button({ className, children, ...rest }: ButtonProps) {
     <button
       {...rest}
       className={cn(
-        "flex w-full items-center justify-center rounded-md bg-green-600 px-4 py-2 text-sm font-medium text-white transition hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-200",
+        "flex w-full items-center justify-center rounded-md bg-green-600 px-4 py-2 text-sm font-medium text-white cursor-pointer transition hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-200",
         className,
       )}
     >
