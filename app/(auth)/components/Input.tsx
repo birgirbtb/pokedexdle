126e371e722fb536ff826541de62dcc7bde4989e
marinofranz <mvbjarnasynir@gmail.com>
2026-01-31 16:58:39 +0000

    apply same design to login/signup pages

--- app/(auth)/components/Input.tsx
+++ app/(auth)/components/Input.tsx
@@ -7,7 +7,7 @@ export default function Input({ className, ...rest }: InputProps) {
     <input
       {...rest}
       className={cn(
-        "w-full rounded-md border border-gray-300 px-3 py-2 text-sm text-gray-900 outline-none transition focus:border-green-500 focus:ring-2 focus:ring-green-200",
+        "w-full rounded-md border border-white/10 px-3 py-2 text-sm text-white outline-none transition focus:border-green-500 focus:ring-2 focus:ring-green-800",
         className,
       )}
     />
