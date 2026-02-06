6ae0f9537b94f71c6039113d01b31ab6ec4285b2
marinofranz <me@marino.codes>
2026-02-06 15:02:07 +0000

    change supabase client to server

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import LogOut from "./(auth)/components/LogOut";
-import { createClient } from "@/lib/supabase/client";
+import { createClient } from "@/lib/supabase/server";
 import Pokedex from "pokedex-promise-v2";
 import Image from "next/image";
 import GameClient from "./components/GameClient";
